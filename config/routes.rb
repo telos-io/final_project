2d429c4fb8c8859d974aa8024695b81ebb011b4b
KoanBrown <koan.brown@gmail.com>
2015-07-14 16:59:26 -0700

    changed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
   resources :codes
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
+  
   root 'codes#index'
 
   resources :users
+
   resources :sessions, only: [:new, :create]
 
 
