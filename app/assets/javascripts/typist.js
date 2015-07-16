ae926226c758ebdc6b8d2a4cdb276ba55d154ca2
KoanBrown <koan.brown@gmail.com>
2015-07-15 20:50:23 -0700

    fixed front end issues

--- app/assets/javascripts/typist.js
+++ app/assets/javascripts/typist.js
@@ -6,17 +6,29 @@ $(document).ready(function(){
 
 // Speed view, first iteration
   var count = 0;
+  var codeArrayCount = 0;
   var inputArray = [];
   var textInput;
-  var inputArray;
   var wordCount = codeArray.length / 5;
   var inputTime;
   var errorCount = 0;
   var errorRate;
 
+  $.each(codeArray, function( key, value ){
+    $('#current-script').append('<span>' + value + '</span>');
+    //codeArrayCount += 1;
+  });
+
   $('.script-input').focus(function(){
     $('#timer').timer();
   });
+
+  // var output = function(inputCount){
+  //   while (var j = 0; j <= codeArray.length; j += 1){
+  //     $('current-script').append('<span>'codeArray[j]'</span>');
+  //   };
+  // };
+
 
   $('.script-input').keyup(function(){
      textInput = $(this).val();
@@ -26,16 +38,20 @@ $(document).ready(function(){
         }else{
           inputArray = textInput.split('');
           count = inputArray.length;
+          $(this).toggleClass("active");
           compare(inputArray, codeArray);
           return count;
       };
   });
 
   function compare(inputArray, codeArray){
+    //$('<span>' + inputArray[count] + '</span>').addClass("active")
         if (inputArray[count - 1] == codeArray[count - 1]){
-          $('.output').append("<span>J</span>");
+          $(inputArray[count - 1]).toggleClass("green");
+          //stays green
         }else{
-          $('.output').append("<span>X</span>");
+          $('#current-script').toggleClass("red");
+          //change red
           errorCount += 1;
         };
         return errorCount;
@@ -46,7 +62,8 @@ $(document).ready(function(){
     inputTime = $('#timer').data('seconds');
 
      if (inputArray.join() == codeArray.join()){
-       compute(inputTime);
+       computeWPM(inputTime);
+       computeErrorRate(errorCount);
        alert("Good Job.  Speed = " + speed + "WPM.  Error Rate = " + errorRate +"%");
      }else{
        alert("Fix it");
@@ -54,14 +71,18 @@ $(document).ready(function(){
      return inputTime;
   });
 
-  function compute(time){
+  function computeWPM(time){
     speed = wordCount / time * 60;
+    return speed;
+  };
+
+  function computeErrorRate(errorCount){
     if (errorCount == 0){
       errorRate = 0;
     }else{
       errorRate = codeArray.length/errorCount;
     }
-    return speed, errorRate;
+    return errorRate;
   };
 
 //compare(inputArray, codeArray);
