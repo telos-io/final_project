fd80698ac215ccfe3a61984a62aceb84ed9c7ea6
KoanBrown <koan.brown@gmail.com>
2015-11-25 14:40:32 -0800

    changed the seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,12 +15,52 @@ exit Minitest.run(ARGV)", "ruby", "Rails Team"],
   var f = i % 3 == 0, b = i % 5 == 0;
   console.log(f ? b ? 'FizzBuzz' : 'Fizz' : b ? 'Buzz' : i);
 }", "javascript", "Paul Irish"],
-["for", "javascript", "Me"],
-["for", "javascript", "Me"],
-["for", "javascript", "Me"],
-["for", "javascript", "Me"],
-["for", "javascript", "Me"],
-["for", "javascript", "Me"]
+["require 'hirb'
+module Hirb::Helpers::Table::Filters
+  def to_milliseconds(seconds)
+    '%.3f ms' % (seconds * 1000)
+  end
+end", "ruby", "change"],
+["def wrap_methods_with_profiling
+      profiler = self
+
+      [
+        { object: @obj.singleton_class, methods: @obj.methods(false), private: false, singleton: true },
+        { object: @obj, methods: @obj.instance_methods(false), private: false },
+        { object: @obj, methods: @obj.private_instance_methods(false), private: true }
+      ].each do |group|
+        group[:object].module_eval do
+          group[:methods].each do |method|
+            define_method('{method}_with_profiling') do |*args, &block|
+              profiler.send(:profile, method, singleton: group[:singleton]) do
+                send('# {method}_without_profiling', *args, &block)
+              end
+            end
+
+            alias_method '# {method}_without_profiling', method
+            alias_method method, '# {method}_with_profiling'
+
+            private '# {method}_with_profiling' if group[:private]
+          end
+        end
+      end
+    end", "ruby", "change"],
+    ["var getRandomArbitrary = function() {
+  return Math.floor(Math.random() * (30 - 0) + 0);
+}
+
+var arr = [0,3,4,5,6,7,9,14,17,24,25,26,29,30];",
+"javascript", "anon"],
+["ModelCore.prototype = {
+            $uuid : null,
+            $pkField : null,
+            $settings : {},
+            $mapping : {},
+            $hasMany : {},
+            $hasOne : {},
+            $dataset : [],
+            $scope : null,
+  }", "javascript", "anon"]
 ]
 
 code_list.each do |script, lang, author|
